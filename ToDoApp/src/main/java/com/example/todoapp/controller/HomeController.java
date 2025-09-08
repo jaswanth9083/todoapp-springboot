d1db31aa7f5d2c85d8d685c2e2cd56cd68537632
jaswanth9083 <jaswanthgudipatisos@gmail.com>
2025-09-08 15:35:07 +0530

    Updated project files and added remaining modules

---
+++ ToDoApp/src/main/java/com/example/todoapp/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.todoapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index"; // resolves to src/main/resources/templates/index.html
+    }
+}
